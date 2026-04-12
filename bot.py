9a5568ebcb02b1d2bfbbb0198f6e42e34c5a86df
Bagir Gubadly <bagir.gubadly@mail.ru>
2026-04-13 02:41:33 +0300

    new workflow + gunicorn

--- bot.py
+++ bot.py
@@ -240,8 +240,6 @@ def set_webhook():
     logger.info("✅ Webhook set")
 
 
-# ========== MAIN ==========
 if __name__ == "__main__":
     set_webhook()
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
